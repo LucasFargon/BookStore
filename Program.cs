5bd03b7d2aa0dd2c8e3116b47c33e6637f0b2ec3
Lucas <lucasfargon2008@gmail.com>
2024-10-30 17:01:27 -0300

    feat: Add connection to database

--- Program.cs
+++ Program.cs
@@ -1,3 +1,6 @@
+using BookStore.Data;
+using Microsoft.EntityFrameworkCore;
+
 namespace BookStore
 {
     public class Program
@@ -9,7 +12,26 @@ namespace BookStore
             // Add services to the container.
             builder.Services.AddControllersWithViews();
 
-            var app = builder.Build();
+
+
+			builder.Services.AddDbContext<BookstoreContext>(options =>
+			{
+				options.UseMySql(
+					builder
+						.Configuration
+						.GetConnectionString("BookstoreContext"),
+					ServerVersion
+						.AutoDetect(
+							builder
+								.Configuration
+								.GetConnectionString("BookstoreContext")
+						)
+				);
+			});
+
+
+
+			var app = builder.Build();
 
             // Configure the HTTP request pipeline.
             if (!app.Environment.IsDevelopment())
