950f5f29fbc52dae865b547257592024fd0d1061
Lucas <lucasfargon2008@gmail.com>
2024-11-25 16:58:35 -0300

    feat: Add bookscontroller's base

---
+++ Services/BookService.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BookStore.Services
+{
+    public class BookService : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
